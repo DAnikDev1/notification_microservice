c2c19e2aacce5c2bf7e5a8128ce4a70735fbe222
danik <danikdevs@gmail.com>
2025-03-30 18:27:06 +0500

    compiling now, but still without exception handler

--- src/main/java/src/danik/notificationservice/NotificationServiceApplication.java
+++ src/main/java/src/danik/notificationservice/NotificationServiceApplication.java
@@ -2,8 +2,10 @@ package src.danik.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "src.danik.notificationservice.config.feign")
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
